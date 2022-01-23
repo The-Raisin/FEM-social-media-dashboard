cb8547ad629910f54b25a5f9d5ec05987f38778a
Ahmed Razaan Adam <razaanadam79@gmail.com>
2022-01-23 17:30:56 +0500

    It's done

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,38 @@
-console.log("Hello world!")
+const darkBtn = document.getElementById('dark');
+const lightBtn = document.getElementById('light');
+
+
+const setColorMode = () => {
+    if (localStorage.getItem("colorMode") == "dark"){
+        setDarkMode();
+        darkBtn.click();
+    } else if (localStorage.getItem("colorMode") == "light"){
+        setLightMode();
+        lightBtn.click();
+    }
+};
+
+const setDarkMode = () => {
+    (document.querySelector("body").classList = "dark");
+}
+
+const setLightMode = () => {
+    (document.querySelector("body").classList = "light");
+}
+
+setColorMode()
+
+const radioBtn = document.querySelectorAll(".toggle__wrapper input");
+
+for (let i = 0; i < radioBtn.length; i++){
+    radioBtn[i].addEventListener("click", event => {
+        if (darkBtn.checked) {
+            localStorage.setItem("colorMode", "dark");
+            setDarkMode();
+        } else {
+            localStorage.setItem("colorMode", "light");
+            setLightMode();
+        }
+    })
+}
+
